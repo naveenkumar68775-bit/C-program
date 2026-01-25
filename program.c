9908049bd1f8cddf5f83f8a1cac28ee7e74ae121
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-25 11:40:35 +0530

    D

--- program.c
+++ program.c
@@ -437,3 +437,13 @@
 //     printf("output:%d",!p);
 //     return 0;
 // }
+
+//5 AND || OR
+// #include<stdio.h>
+// int main()
+// {
+//     int p,q;
+//     scanf("%d%d",&p,&q);
+//     printf("output:%d",p||q);
+//     return 0;
+// }
