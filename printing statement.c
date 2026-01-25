5b351eef4ab75de2c3fd34ce0f590be1af8f7f60
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-25 13:54:55 +0530

    va

--- printing statement.c
+++ printing statement.c
@@ -1,10 +1,66 @@
 //TOPIC 1
-#include<stdio.h>
-int main()
-{
-    printf("Hello World");
-    return 0;
-}
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World");
+//     return 0;
+// }
+
+//2
+// int main(){
+//     printf("Gojo");
+//     return 0;
+// }
+
+//3
+// int main()
+// {
+//     int a=10,b=25;
+//     printf("%d",a);
+//     printf("\n%d",b);
+//     return 0;
+// }
+
+//4
+// int main(){
+//     printf("Output:%.1f",3.5);
+//     printf("\nOutput:%.2f",7.25);
+//     return 0;
+// }
+
+//5
+// int main()
+// {
+//     char ch='A',gh='Z';
+//     printf("output:%c",ch);
+//     printf("\noutput:%c",gh);
+//     return 0;
+// }
+
+//6
+// int main()
+// {
+//     int a=5,b=10;
+//     printf("output:%d %d",a,b);
+//     return 0;
+// }
+
+//7
+// int main(){
+//     printf("Output:Age:%d\n",20);
+//     printf("Output:Count:%d",5);
+//     return 0;
+// }
+
+//8
+// int main()
+// {
+//     printf("Good");
+//     printf("\nMorning\n");
+//     printf("\nHello");
+//     printf("\nWorld");
+//     return 0;
+// }
 
 
 
