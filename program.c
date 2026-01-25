38b5031f228ff7969a768b9e351fd9be0eaa2736
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-25 11:45:32 +0530

    J

--- program.c
+++ program.c
@@ -456,3 +456,12 @@
 //     printf("Output:%d",a);
 //     return 0;
 // }
+
+//2 +=
+// #include<stdio.h>
+// int main(){
+//     int a,s;
+//     scanf("%d%d",&a,&s);
+//     printf("Output:%d",a+=s);
+//     return 0;
+// }
