545f53a3ca375b2b362e960344b73085aade3537
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-25 13:51:24 +0530

    comm

--- variables .c
+++ variables .c
@@ -1,131 +1,131 @@
-TOPIC 2
+// TOPIC 2
 
-1
-#include<stdio.h>
-int main()
-{
-    int a=10,b=50;
-    printf("output:%d",a);
-    printf("\noutput:%d",b);
-    return 0;
-}
+// 1
+// #include<stdio.h>
+// int main()
+// {
+//     int a=10,b=50;
+//     printf("output:%d",a);
+//     printf("\noutput:%d",b);
+//     return 0;
+// }
 
-2
-int main()
-{
-    int a=5,b=15;
-    printf("Output:%d %d",a,b);
-    return 0;
-}
+// 2
+// int main()
+// {
+//     int a=5,b=15;
+//     printf("Output:%d %d",a,b);
+//     return 0;
+// }
 
-3
-int main(){
-    float p=2.5,q=6.75;
-    printf("Output:%.1f",p);
-    printf("\nOutput:%.2f",q);
-    return 0;
-}
+// 3
+// int main(){
+//     float p=2.5,q=6.75;
+//     printf("Output:%.1f",p);
+//     printf("\nOutput:%.2f",q);
+//     return 0;
+// }
 
-4
-int main()
-{
-    char c='X',h='B';
-    printf("output:%c",c);
-    printf("\noutput:%c",h);
-    return 0;
-}
+// 4
+// int main()
+// {
+//     char c='X',h='B';
+//     printf("output:%c",c);
+//     printf("\noutput:%c",h);
+//     return 0;
+// }
 
-5
-int main(){
-    int n=50;
-    printf("output:%d",n);
-    n=100;
-    printf("\noutput:%d",n);
-}
+// 5
+// int main(){
+//     int n=50;
+//     printf("output:%d",n);
+//     n=100;
+//     printf("\noutput:%d",n);
+// }
 
-6
-int main()
-{
-    int a=5,b=10;
-    printf("output:%d",a+b);
-    int c=20+10;
-    printf("\noutput:%d",c);
-    return 0;
-}
+// 6
+// int main()
+// {
+//     int a=5,b=10;
+//     printf("output:%d",a+b);
+//     int c=20+10;
+//     printf("\noutput:%d",c);
+//     return 0;
+// }
 
-7
-int main()
-{
-    int a=10,b=5;
-    printf("output:%d",a-b);
-    a=20;
-    b=10;
-    printf("\nOutput:%d",a-b);
-    return 0;
-}
+// 7
+// int main()
+// {
+//     int a=10,b=5;
+//     printf("output:%d",a-b);
+//     a=20;
+//     b=10;
+//     printf("\nOutput:%d",a-b);
+//     return 0;
+// }
 
-8
-int main()
-{
-    int a=4,b=5;
-    printf("Output:%d",a*b);
-    int c=10,d=5;
-    printf("\nOutput:%d",c*d);
-    return 0;
-}
+// 8
+// int main()
+// {
+//     int a=4,b=5;
+//     printf("Output:%d",a*b);
+//     int c=10,d=5;
+//     printf("\nOutput:%d",c*d);
+//     return 0;
+// }
 
-9
-int main()
-{
-    int a=20,b=5;
-    printf("Output:%d",a/b);
-    int c=50,d=10;
-    printf("\nOutput:%d",c/d);
-}
+// 9
+// int main()
+// {
+//     int a=20,b=5;
+//     printf("Output:%d",a/b);
+//     int c=50,d=10;
+//     printf("\nOutput:%d",c/d);
+// }
 
-TOPIC ASSIGNMENT OPERATOR
-#include<stdio.h>
-int main(){
-    int a;
-    scanf("%d",&a);
-    printf("Output:%d",a);
-    return 0;
-}
+// TOPIC ASSIGNMENT OPERATOR
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     scanf("%d",&a);
+//     printf("Output:%d",a);
+//     return 0;
+// }
 
-2 +=
-#include<stdio.h>
-int main(){
-    int a,s;
-    scanf("%d%d",&a,&s);
-    printf("Output:%d",a+=s);
-    return 0;
-}
+// 2 +=
+// #include<stdio.h>
+// int main(){
+//     int a,s;
+//     scanf("%d%d",&a,&s);
+//     printf("Output:%d",a+=s);
+//     return 0;
+// }
 
-3  -=
-#include<stdio.h>
-int main(){
-    int a,s;
-    scanf("%d%d",&a,&s);
-    printf("Output:%d",a-=s);
-    return 0;
-}
+// 3  -=
+// #include<stdio.h>
+// int main(){
+//     int a,s;
+//     scanf("%d%d",&a,&s);
+//     printf("Output:%d",a-=s);
+//     return 0;
+// }
 
-4  *=
-#include<stdio.h>
-int main(){
-    int a,s;
-    scanf("%d%d",&a,&s);
-    printf("Output:%d",a*=s);
-    return 0;
-}
+// 4  *=
+// #include<stdio.h>
+// int main(){
+//     int a,s;
+//     scanf("%d%d",&a,&s);
+//     printf("Output:%d",a*=s);
+//     return 0;
+// }
 
-5  /= and %=
-#include<stdio.h>
-int main(){
-    int a,s;
-    scanf("%d%d",&a,&s);
-    int c=a/s;
-    int d=a%=s;
-    printf("Output:%d %d",c,d);
-    return 0;
-}
+// 5  /= and %=
+// #include<stdio.h>
+// int main(){
+//     int a,s;
+//     scanf("%d%d",&a,&s);
+//     int c=a/s;
+//     int d=a%=s;
+//     printf("Output:%d %d",c,d);
+//     return 0;
+// }
