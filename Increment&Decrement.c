84d86314301d19d0c2fade0b8ab0aa54eb074966
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-25 13:59:07 +0530

    VALUE

--- Increment&Decrement.c
+++ Increment&Decrement.c
@@ -1,57 +1,56 @@
-//TOPIC: Increment/Decrement operator
 
-//1 pre increment
-// #include<stdio.h>
-// int main()
-// {
-//     int n;
-//     printf("Input:");
-//     scanf("%d",&n);
-//     n++;
-//     printf("Output:%d",n);
-//     return 0;
-// }
 
-//2 post increment
-// int main()
-// {
-//     int a;
-//     printf("Input:");
-//     scanf("%d",&a);
-//     printf("Output:%d",a++);
-//     return 0;
-// }
+1 pre increment
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Input:");
+    scanf("%d",&n);
+    n++;
+    printf("Output:%d",n);
+    return 0;
+}
 
-//3 pre decrement
-// int main()
-// {
-//     int r;
-//     printf("Input:");
-//     scanf("%d",&r);
-//     printf("Output:%d",--r);
-//     return 0;
-// }
+2 post increment
+int main()
+{
+    int a;
+    printf("Input:");
+    scanf("%d",&a);
+    printf("Output:%d",a++);
+    return 0;
+}
 
-//4 increment twice
+3 pre decrement
+int main()
+{
+    int r;
+    printf("Input:");
+    scanf("%d",&r);
+    printf("Output:%d",--r);
+    return 0;
+}
 
-// int main()
-// {
-//     int r;
-//     printf("Input:");
-//     scanf("%d",&r);
-//     r=++r;
-//     printf("Output:%d",++r);
-//     return 0;
-// }
+4 increment twice
 
-//5 decrement twice
-// int main()
-// {
-//     int v;
-//     printf("Input:");
-//     scanf("%d",&v);
-//     v=--v;
-//     printf("Output:%d",--v);
-//     return 0;
-// }
-//
+int main()
+{
+    int r;
+    printf("Input:");
+    scanf("%d",&r);
+    r=++r;
+    printf("Output:%d",++r);
+    return 0;
+}
+
+5 decrement twice
+int main()
+{
+    int v;
+    printf("Input:");
+    scanf("%d",&v);
+    v=--v;
+    printf("Output:%d",--v);
+    return 0;
+}
