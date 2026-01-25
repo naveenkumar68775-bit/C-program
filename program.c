4262228b43c41441c3302eb337554bdb8b6739dc
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-25 11:28:50 +0530

    Add

--- program.c
+++ program.c
@@ -388,10 +388,22 @@
 // }
 
 //5  >= && =<
-#include<stdio.h>
-int main(){
-    int a,b;
-    scanf("%d%d",&a,&b);
-    printf("output:a=%d b=%d",a>=b , a<=b);
-    return 0;
-}
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     scanf("%d%d",&a,&b);
+//     printf("output:a=%d b=%d",a>=b , a<=b);
+//     return 0;
+// }
+
+//Logical 
+
+//1  AND
+// #include<stdio.h>
+// int main()
+// {
+//     int p,q;
+//     scanf("%d%d",&p,&q);
+//     printf("output:%d",p&&q);
+//     return 0;
+// }
