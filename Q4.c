e83a39940e0e58be070fdfcbb58565a43ba73203
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-26 09:23:31 +0530

    Com

---
+++ Q4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,k;
+    scanf("%d%d",&n,&k);
+    if(n>k){
+        printf("%d is greater",n);
+    }
+    else{
+        printf("%d is greater",k);
+    }
+    return 0;
+}
