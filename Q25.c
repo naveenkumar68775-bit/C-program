eddc6bd4ea6bfe8909b048a7d07d0d3b03c15b39
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-26 17:21:59 +0530

    c program

---
+++ Q25.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int  a,b;
+    printf("Enter:");
+    scanf("%d%d\n",&a,&b);
+    printf("1.ADD\n");
+    printf("2.SUB\n");
+    printf("3,mul\n");
+    printf("4.div\n");
+    int t;
+    scanf("%d",&t);
+    switch(t){
+        case 1:
+            printf("sum=%d",a+b);
+            break;
+        case 2:
+            printf("sum=%d",a-b);
+            break;
+        case 3:
+            printf("sum=%d",a*b);
+            break;
+        case 4:
+            printf("sum=%d",a/b);
+            break;
+        default:
+            printf("Invalid");
+        }
+}
