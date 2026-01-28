a97ce465d027d5ea6d73aeb9efb7c166b1adaa82
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-28 15:20:44 +0530

    Value

---
+++ for 10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=0,b=1,n,c;
+    printf("input:");
+    scanf("%d",&n);
+    printf("%d %d",a,b);
+    for( int i=2;i<n;i++){
+        c=a+b;
+        printf(" %d",c);
+        a=b;
+        b=c;
+    }
+    return 0;
+     
+}
