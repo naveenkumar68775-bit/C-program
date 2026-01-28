35d1f01eb09836a89524e164347cd59ec195c993
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-28 16:44:52 +0530

    Value

---
+++ for 13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,s=0;
+    scanf("%d%d",&a,&b);
+    for (int i=1;i<=b;i++){
+        if(i%a==0){
+            s=s+i;
+           
+        }
+            
+    }
+    printf("%d",s);
+    return 0;
+}
