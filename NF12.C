29dbc310574e96bde8bf7f2034a14493a325de93
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-29 13:59:59 +0530

    Value

--- NF12.C
+++ NF12.C
@@ -6,9 +6,9 @@ int main()
             printf("%d",k);
         }
         for (int j=1;j<=7-i;j++){
-            printf(" ");
+            printf("  ");
         }
-        for(int v=i;v<=2;v++){
+        for(int v=1;v<=2*i-1;v++){
             printf("%d",v);  
         }
     }
