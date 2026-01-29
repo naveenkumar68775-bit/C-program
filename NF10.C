988ba7d141c6a72e48bc1a465b8c4895e39ca7f3
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-29 11:23:16 +0530

    Value

---
+++ NF10.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int k=4;k>0;k--){
+        for(int l=1;l<=k;l++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
