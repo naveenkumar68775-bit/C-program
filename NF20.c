df27b78f6bd0c34e36d8511c3fc6f2cc7841fcfe
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-01-29 22:02:55 +0530

    loop 20

--- NF20.c
+++ NF20.c
@@ -1,22 +1,25 @@
-#include<stdio.h>
-int main()
-{
-    #include<stdio.h>
-int main()
-{
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=i;j++){
-            printf("*");
-        }
-        printf("\n");
-    }
-    for(int k=4;k>0;k--){
-        for(int l=1;l<=k;l++){
-            printf("*");
-        }
-        printf("\n");
-    }
-    return 0;
-}
+#include <stdio.h>
 
+int main() {
+    int n = 5;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=i;j++)
+            printf("*");
+        for (int j=1;j<=2*(n-i);j++)
+            printf(" ");
+        for (int j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+    for (int i=n-1; i>=1;i--) {
+        for (int j=1;j<=i;j++)
+            printf("*");
+        for (int j=1;j<=2*(n-i);j++)
+            printf(" ");
+        for (int j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
 }
